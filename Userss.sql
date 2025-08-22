673e9fdc9543f72cff9463def1fe1fe8553fff24
Akif Ismayilov <thisisfear@internet.ru>
2025-08-22 16:57:38 +0400

    Userss.sql File Added

---
+++ Userss.sql
@@ -0,0 +1,19 @@
+﻿
+CREATE DATABASE Userss
+GO
+
+USE Userss
+GO
+
+
+CREATE TABLE [dbo].[Users] (
+    [Id]        INT            IDENTITY (1, 1) NOT NULL,
+    [UserName]  NVARCHAR (50)  NOT NULL,
+    [Password]  NVARCHAR (100) NOT NULL,
+    [FirstName] NVARCHAR (50)  NOT NULL,
+    [LastName]  NVARCHAR (50)  NOT NULL,
+    [Age]       INT            NOT NULL,
+    [Gender]    BIT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    UNIQUE NONCLUSTERED ([UserName] ASC)
+)
